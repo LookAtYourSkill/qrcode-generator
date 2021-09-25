ed5e65057144eec3d170a3ca396db0b431441225
LookAtYourSkill <66884858+LookAtYourSKill@users.noreply.github.com>
2021-09-26 00:32:58 +0200

    Create qrcode generator.py

---
+++ qrcode generator.py
@@ -0,0 +1,14 @@
+import qrcode as qrcode
+
+input_data = input('Für welchen Link soll ein QR Code generiert werden?\n')
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(str(input_data))
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+name = input('Wie soll der QR Code heißen?\n')
+img.save('C:/Users/simon/Documents/QR Codes from Generator/' + str(name) + '.png')
+print('QR Code Successfully created\n'
+      'Du hast dich für ' + str(input_data) + ' entscheiden!')
